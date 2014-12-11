af48c7bab6eba9942daa8deeec2303bceddea714
Mattis Kancans Envall <mattiskan@gmail.com>
2014-12-11 12:29:53 +0100

    added farm test

---
+++ tests/test_farm.h
@@ -0,0 +1,34 @@
+// -*-c++-*-
+#include <string>
+#include <iostream>
+#include <cxxtest/TestSuite.h>
+#include <jsoncpp/json.h>
+
+#include "../src/point.h"
+#include "../src/farm.h"
+
+using namespace Novia;
+
+class TestFarm : public CxxTest::TestSuite
+{
+ public:
+
+  void test_create() {
+    Farm farm(Point (1,2));
+  }
+
+  void test_serialize() {
+    Farm farm(Point (1,2));
+
+    return; // TODO
+
+    const char* expected = "todo";
+
+    Json::FastWriter writer;
+
+    std::string output = writer.write(farm.serialize());
+    TS_ASSERT_EQUALS(output, expected);
+  }
+
+
+};
