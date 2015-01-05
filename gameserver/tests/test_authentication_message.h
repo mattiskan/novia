983956b077f6860c1b9ac0a5f46598842fd65687
Mattis Kancans Envall <mattiskan@gmail.com>
2015-01-05 19:24:21 +0100

    added code to lookup message type

---
+++ gameserver/tests/test_authentication_message.h
@@ -0,0 +1,31 @@
+// -*-c++-*-
+#include <cxxtest/TestSuite.h>
+//#include <jsoncpp/json.h>
+#include "../src/protocol/authentification_message.h"
+
+using namespace novia;
+
+class TestAuthenticationMessage : public CxxTest::TestSuite
+{
+ public:
+  void test_creation() {
+    Json::Value auth;
+    Json::Reader reader;
+
+    const char* txt =
+      "{"
+       "\"username\": \"bob\","
+       "\"password\": \"banana\""
+      "}";
+
+    reader.parse(txt, auth, false);
+        
+    AuthentificationMessage msg;
+    msg.read(auth);
+
+    TS_ASSERT_EQUALS(msg.username(), "bob");
+    TS_ASSERT_EQUALS(msg.password(), "banana");
+    
+  }
+
+};
