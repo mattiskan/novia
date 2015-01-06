b628bccf7eccae612292693b9ac5f8b607e47705
Mattis Kancans Envall <mattiskan@gmail.com>
2015-01-06 20:19:54 +0100

    server now supports request map_requests

---
+++ gameserver/src/protocol/request_map_message.cpp
@@ -0,0 +1,32 @@
+//-*-c++-*-
+#include "request_map_message.h"
+
+namespace novia {
+
+  void RequestMapMessage::read(const Json::Value& data) {
+    
+  }
+
+  void RequestMapMessage::instant_reply(const Controllers& c,
+					ClientConnection& owner) const {
+
+    Json::Value serialized_map = c.map_controller.get_serialized();
+    RequestMapResponse response(serialized_map);
+    owner.send(&response);
+  }
+  
+  void RequestMapMessage::on_invoke(Controllers& c,
+				    ClientConnection& owner) const {
+
+  }
+
+  RequestMapResponse::RequestMapResponse(Json::Value& serialized_map)
+    : serialized_map_(serialized_map) {
+
+  }
+  
+  const Json::Value RequestMapResponse::get_message() const {
+    return serialized_map_;
+  }
+}
+
