2cd4e13ea22936a92497e55ba21cf1167f8afc25
Mattis Kancans Envall <mattiskan@gmail.com>
2015-01-08 15:51:32 +0100

    made login controller keep track of whether it has connected to a server

--- web-client/js/app.js
+++ web-client/js/app.js
@@ -15,18 +15,19 @@ app.factory('socketFactory', ['$rootScope', function($rootScope) {
     var has_connected = false;
     
     return {
-	connect: function(ip, auth_msg) {
+	connect: function(ip, auth_msg, callback) {
 	    ws = new WebSocket("ws://"+ ip +":9002");
 	    
 	    ws.onopen = function() {
 		has_connected = true;
 		console.log("Connection successful");
 		ws.send(JSON.stringify(auth_msg))
-	    }
+		callback();
+	    };
 	    ws.onclose = function() {
 		if(has_connected)
 		    console.log("Socket closed");
-	    }
+	    };
 	},
 	onMessage : function(callback) {
 	    ws.onmessage = function(msg) {
